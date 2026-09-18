b7c21f64baf710d4783056263d01ca3d072d4d3f
4nten0r <antenorhpfiuza@gmail.com>
2026-09-18 09:56:51 -0300

    Configura planilha existente da aplicacao

--- api-config.js
+++ api-config.js
@@ -1,4 +1,4 @@
 // Configuração pública da instalação atual.
 // Não coloque credenciais de serviços privados neste arquivo.
-window.LAVAPET_API_URL = '';
+window.LAVAPET_API_URL = 'https://script.google.com/macros/s/AKfycbwE8I4T1FtPBEt7VZ6jJ_06mRBuQPxSKMQE5USswJ2jvnEErhtN5oQAB3cdjiM788wDVw/exec';
 window.LAVAPET_API_TOKEN = '';
